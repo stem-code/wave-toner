c3a4efbe29ed0ee9853042a003d909f740eeb76f
dorianSTEM <dorian.cauwe@s.stemk12.org>
2019-03-06 12:34:43 -0700

    Fixes

--- public/src/app/tone-graph/tone-graph.component.ts
+++ public/src/app/tone-graph/tone-graph.component.ts
@@ -468,11 +468,11 @@ export class ToneGraphComponent implements OnInit {
   }
 
   setWaveData(event) {
+    $('app-file-window').css({'z-index': -5000, 'opacity': 0});
     console.log(event);
     if (!event) {
        return 0;
     }
     this.waves = event.waves;
-    $('app-file-window').css({'z-index': -5000, 'opacity': 0});
   }
 }
